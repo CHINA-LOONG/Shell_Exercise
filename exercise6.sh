19b5106830b4965a32936815e3b88f7de2605fec
loong <loong@study.centos.loong>
2019-02-17 21:01:08 +0800

    [add file]--exercise7/8/9/10--

--- exercise6.sh
+++ exercise6.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 
+echo "Process ID: $$"
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "All parameters 1: $@"
+echo "All parameters 2: $*"
+echo "Total: $#"
+
+
+
+
+
+
+
 
 
